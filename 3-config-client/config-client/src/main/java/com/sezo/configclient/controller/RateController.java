538a7fe41e809ba047c2eedb499ccd32e5a60db6
Sezayir Dagtekin <sezayir.dagtekin@globallogic.com>
2023-04-13 16:49:39 +0200

    consume config server with cloud client

---
+++ 3-config-client/config-client/src/main/java/com/sezo/configclient/controller/RateController.java
@@ -0,0 +1,29 @@
+package com.sezo.configclient.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RateController {
+
+	@Value("${rate}")
+	String rate;
+
+	@Value("${tollstart}")
+	String tollstart;
+
+	@Value("${lanecount}")
+	String lanecount;
+
+	@GetMapping("/rate")
+	public String getRate(Model m) {
+		m.addAttribute("rateamount", rate);
+		m.addAttribute("tollstart", tollstart);
+		m.addAttribute("lanes", lanecount);
+
+		return "rateview";
+
+	}
+}
